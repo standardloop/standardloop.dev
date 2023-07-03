c20741fcc16e92b8a9aa6e96ede9cbda73e498bb
jbrard <Joshuaeli1234@gmail.com>
2023-07-03 11:33:25 -0600

    commit before animation delay

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,29 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        blob: "blob 30s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(0.0)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(0.66)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(1.32)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(0.0)",
+          },
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+        //   'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
