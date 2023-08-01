71c882d5548b19978635c7d12310dad20538873d
jbrard <Joshuaeli1234@gmail.com>
2023-07-31 20:57:15 -0600

    Added: security headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
     poweredByHeader: false,
     reactStrictMode: true,
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    { key: "Content-Security-Policy", value: "frame-ancestors 'self'" },
+                    { key: "Referrer-Policy", value: "same-origin" },
+                    { key: "X-Frame-Options", value: "deny" },
+                    { key: "Strict-Transport-Security", value: "max-age=31536000;includeSubDomains" },
+                    { key: "X-Content-Type-Options", value: "nosniff" },
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const socials = {
+    "medium": "https://medium.com/@standardloop",
+    "instagram": "https://www.instagram.com/standardloop/",
+    "youtube": "https://www.youtube.com/@standardloop"
+}
