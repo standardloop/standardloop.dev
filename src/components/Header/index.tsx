76dfdae363652264f80f83164b357a0bad672160
jbrard <Joshuaeli1234@gmail.com>
2023-10-28 10:46:36 -0600

    starbucks header progress

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Link from 'next/link'
 
 export default function Header() {
     return (
         <React.Fragment>
             <header>
-                <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+                <nav className="bg-emerald-500 border-gray-200 px-4 lg:px-6 py-2.5 cursor-default">
                     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                        <a href="https://flowbite.com" className="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                        </a>
+                        <Link href="/" className="flex items-center">
+                            <img src="/favicon.ico" className="mr-3 h-6 sm:h-9 cursor-pointer" alt="Logo" />
+                            <div className="cursor-pointer self-center text-xl font-semibold whitespace-nowrap">
+                                standardloop
+                            </div>
+                        </Link>
                         <div className="flex items-center lg:order-2">
                             <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
                             <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Get started</a>
